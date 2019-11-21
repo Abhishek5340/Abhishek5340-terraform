4fdc24271eb4ec5b7edf2b7f87213b9e419aae36
Abhishek5340 <57651063+Abhishek5340@users.noreply.github.com>
2019-11-21 11:12:19 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-module "Abhi-pipeline" {
-    source = "https://github.com/upesabhi/terraform.git"
+# module "Abhi-pipeline" {
+#     source = "https://github.com/upesabhi/terraform.git"
 
-}
+# }
 
 variable "Abhishek5340" {
    default = "This is Sparta"
 }
 
 output "Abhi-OutPipe" {
-    value = module.Abhi-pipeline.story + var.Abhishek5340
+    value = var.Abhishek5340
 }
